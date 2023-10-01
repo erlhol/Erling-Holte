762b535cb48729ad76144133851578be32ca58e3
Erling Holte <erling.holte@outlook.com>
2023-10-01 12:23:16 +0200

    Switch to React

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <header>
+      <nav>
+        <a href="index.html">About me</a>
+        <a href="projects.html">My current projects</a>
+      </nav>
+    </header>
+
+    <h1>Innhold kommer her</h1>
+
+    <footer>
+      <p>Måter du kan kontakte meg: </p>
+      <a href="mailto:erlinhol@uio.no">Mail</a>
+    </footer>
+    </div>
+  );
+}
+
+export default App;
