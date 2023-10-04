7b3747c35615a469a179007294b502b78abfa023
Erling Holte <erling.holte@outlook.com>
2023-10-04 17:38:58 +0200

    Add more detailed start end date for workexperience

--- my-app/src/components/Work.js
+++ my-app/src/components/Work.js
@@ -28,12 +28,12 @@ function Work() {
     } 
 
     const workExperience = 
-    [ [ "2019-2020", "Førstegangstjeneste HMKG","Geværsoldat hos Hans Majestet Kongens Garde. 2. gardekompani",forsvaret_logo],
-    [ "2022", "Frivillig IMO", "Var guide for Peru under den 63. internasjonale matematiske olympiaden som ble holdt i Oslo fra 10. til 15. juli",imo_logo],
-    [ "2021-2022","Vitenvert", "Jobbet med arrangering og gjennomføring av sommerskole og andre arrangementer i regi av Vitenparken i Ås.",vitenparken_logo],
-    [ "2022", "Butikkmedarbeider", "Ekstrahjelp julen 2022 som butikkmedarbeider hos Norli i Lillestrøm",norli_logo],
-    [ "2021-D.D","Gruppelærer","Stillingen innebærer forberedelse og gjennomføring av undervisning gjennom gruppetimer og retting av obligatoriske oppgaver.",uio_logo],
-    ["2020-2023","Kormedarbeider","Hentet barna på skolen og fulgte dem til øvelseslokalet, skar opp frukt og hjalp til med diverse, holdt øvelse dersom dirigenten var borte.",lillestrom_kirke_logo]]
+    [ [ "AUGUST 2021-D.D.","Gruppelærer","Stillingen innebærer forberedelse og gjennomføring av undervisning gjennom gruppetimer og retting av obligatoriske oppgaver.",uio_logo],
+    [ "OKTOBER 2022 – JANUAR 2023", "Butikkmedarbeider", "Ekstrahjelp julen 2022 som butikkmedarbeider hos Norli i Lillestrøm",norli_logo],
+    [ "JULI 2022", "Frivillig IMO", "Var guide for Peru under den 63. internasjonale matematiske olympiaden som ble holdt i Oslo fra 10. til 15. juli",imo_logo],
+    [ "JUNI 2021 – AUGUST 2022","Vitenvert", "Jobbet med arrangering og gjennomføring av sommerskole og andre arrangementer i regi av Vitenparken i Ås.",vitenparken_logo],
+    [ "AUGUST 2019 – JULI 2020", "Førstegangstjeneste HMKG","Geværsoldat hos Hans Majestet Kongens Garde. 2. gardekompani",forsvaret_logo],
+    ["AUGUST 2020 – JUNI 2023","Kormedarbeider","Hentet barna på skolen og fulgte dem til øvelseslokalet, skar opp frukt og hjalp til med diverse, holdt øvelse dersom dirigenten var borte.",lillestrom_kirke_logo]]
     return (
         <>
         {workExperience.map((value, i) => (
