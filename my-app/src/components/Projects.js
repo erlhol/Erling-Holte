d0c5ff890ba976d72cf3ac27a109badaf0a7e71f
Erling Holte <erling.holte@outlook.com>
2023-10-04 17:34:28 +0200

    Switch to .js for Projects

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Projects: React.FC = () => {
+function Projects() {
 
-    const myProjects: Array<Array<String>> = [ [ "2021", "Universitetet i Oslo","Bruksorientert design"],
+    const myProjects = [ [ "2021", "Universitetet i Oslo","Bruksorientert design"],
                                              [ "2022", "Universitetet i Oslo", "Software Engineering med prosjektarbeid"],
                                              [ "2023","Eget prosjekt", "EmnePlanleggern"],
                                              [ "2016-2019", "Lillestrøm Videregående skole", "Studiespesialisering med fordypning i realfag"]]
