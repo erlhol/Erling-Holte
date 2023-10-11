9d2252c81ed186b559f497209a348d6e228424ce
Erling Holte <erling.holte@outlook.com>
2023-10-11 17:03:00 +0200

    Add gridlayout

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -6,7 +6,7 @@ function Projects() {
     [ "2022", "Universitetet i Oslo", "IN2000 - Software Engineering med prosjektarbeid","https://github.com/erlhol/Oscyclo-IN2000"],
                          [ "2021", "Universitetet i Oslo","IN1060 - Bruksorientert design","https://www.uio.no/studier/emner/matnat/ifi/IN1060/v21/prosjekter-var-2021/dorstokkmila/"]]
 
-    return (<div style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+    return (<div style={{display: 'grid', gridTemplateColumns: 'auto auto', gap: '20px'}}>
         {myProjects.map((values,i) => (
             <div className="content" key={i} style={{wordBreak: 'break-all'}}>
                 <p>{values[1]} - {values[0]}</p>
