2613eadd45261d65f5f8876f8e4168b9e4e4dba9
Erling Holte <erling.holte@outlook.com>
2024-08-19 21:59:35 +0200

    Apply pretty formatting

--- my-app/src/components/Contact.tsx
+++ my-app/src/components/Contact.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Contact: React.FC = () => {
     return (<><p>Kontakt meg gjerne på mail: </p>
-    <a href="mailto:erlinhol@uio.no">erlinhol@uio.no</a></>)
+        <a href="mailto:erlinhol@uio.no">erlinhol@uio.no</a></>)
 };
 
 export default Contact;
--- my-app/src/pages/AboutMe.tsx
+++ my-app/src/pages/AboutMe.tsx
@@ -4,19 +4,19 @@ import moment from 'moment';
 const AboutMe: React.FC = () => {
     const years = moment().diff('2000-12-01', 'years');
     return (<div><p>Jeg er en {years} år gammel informatikkstudent på Universitetet i Oslo.</p>
-    <p>På denne siden kan du lese om min erfaring og mine prosjekter.</p>
-    <p>Jeg kommer fra Lillestrøm og har mange interesser (i tillegg til programmering 😁 ) </p>
-    <div style={{textAlign: 'center'}}>
-        <ul style={{display: 'inline-block'}}>
-            <li>Fotball ⚽</li>
-            <li>Lesing📖</li>
-            <li>Piano 🎹</li>
-            <li>Sang🎶</li>
-            <li>Dans🕺</li>
-            <li>Løping🏃🏻‍♂️</li>
-            <li>Å reise🗺️</li>
-        </ul>
-    </div>
+        <p>På denne siden kan du lese om min erfaring og mine prosjekter.</p>
+        <p>Jeg kommer fra Lillestrøm og har mange interesser (i tillegg til programmering 😁 ) </p>
+        <div style={{ textAlign: 'center' }}>
+            <ul style={{ display: 'inline-block' }}>
+                <li>Fotball ⚽</li>
+                <li>Lesing📖</li>
+                <li>Piano 🎹</li>
+                <li>Sang🎶</li>
+                <li>Dans🕺</li>
+                <li>Løping🏃🏻‍♂️</li>
+                <li>Å reise🗺️</li>
+            </ul>
+        </div>
 
     </div>);
 };
